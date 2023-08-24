9f1f43b2815a40efe03b3ac8f6fecde734c42af4
gabbrenn <gabvladimirbrenn@gmail.com>
2023-08-24 13:42:02 -0700

    Stacks, Queues - LIFO, FIFO

--- monty.h
+++ monty.h
@@ -2,11 +2,7 @@
 #define MONTY_H
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <unistd.h>
-#include <fcntl.h>
 #include <string.h>
-#include <ctype.h>
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -22,22 +18,6 @@ typedef struct stack_s
 	struct stack_s *prev;
 	struct stack_s *next;
 } stack_t;
-/**
- * struct bus_s - variables -args, file, line content
- * @arg: value
- * @file: pointer to monty file
- * @content: line content
- * @lifi: flag change stack <-> queue
- * Description: carries values through the program
- */
-typedef struct bus_s
-{
-	char *arg;
-	FILE *file;
-	char *content;
-	int lifi;
-}  bus_t;
-extern bus_t bus;
 /**
  * struct instruction_s - opcode and its function
  * @opcode: the opcode
